3f044da88ca93bbf2c10ec5da4938feeb58ff5c6
juanmism <juanmism@gmail.com>
2022-01-18 13:39:18 +0000

    end

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,11 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let count = 0;
+setInterval(() => {
+	ReactDOM.render(<Home count={count} />, document.querySelector("#app"));
+	count++;
+}, 1000);
+
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
